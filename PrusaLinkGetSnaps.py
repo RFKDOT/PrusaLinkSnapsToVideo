c8a31e10c20d024d6265a0d1c7e3b25984772e56
RFK <rafakroma@gmail.com>
2023-08-20 16:46:36 +0200

    Some changes and improvements

--- PrusaLinkGetSnaps.py
+++ PrusaLinkGetSnaps.py
@@ -8,27 +8,17 @@ load_dotenv()
 
 IP_ADDRESS = os.getenv("PRUSALINK_IP")
 API_KEY = os.getenv("PRUSALINK_API_KEY")
-DEBUG = os.getenv("DEBUG", 1)
+DEBUG = bool(os.getenv("DEBUG", 1))
 SLEEP_TIME = int(os.getenv("SLEEP_TIME", 1))
 
 HEADERS = {"X-Api-Key": API_KEY}
 
 sleep_time = SLEEP_TIME
 previous_snapshot = None
-snapshot_counter = 0
 
 def this_time():
     now = datetime.now()
     return now.strftime("%d/%m/%Y %H:%M:%S")
-
-def initialize():
-    try:
-        info_response = requests.get(f"http://{IP_ADDRESS}/api/v1/info", headers=HEADERS)
-        info_response.raise_for_status()
-        return True
-    except Exception as e:
-        print(f"{this_time()} Error: {e}", flush=True)
-        return False
 
 def format_time(seconds):
     """Format time in seconds to HH:MM:SS"""
@@ -37,18 +27,21 @@ def format_time(seconds):
     return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 def download_snapshot():
-    global previous_snapshot
-    global sleep_time
+    global previous_snapshot, sleep_time
 
-    job_response = requests.get(f"http://{IP_ADDRESS}/api/v1/job", headers=HEADERS)
-    job_response.raise_for_status()
+    try:
+        job_response = requests.get(f"http://{IP_ADDRESS}/api/v1/job", headers=HEADERS)
+        job_response.raise_for_status()
+    except Exception as e:
+         print(f"{this_time()} Parece que en este momento la impresora está apagada \n {e}", flush=True)
+         sleep_time = 300
+         return
 
     if job_response.status_code != 200:
-        print(f"{this_time()} Parece que en este momento no se está imprimiendo nada", flush=True)
+        print(f"{this_time()} Parece que en este momento no se está imprimiendo nada \n {job_response.status_code}", flush=True)
         sleep_time = 120
         return
 
-    # print(job_response.json())
     job_data = job_response.json()
 
     job_name = job_data.get("file", {}).get("name", "")
@@ -62,7 +55,11 @@ def download_snapshot():
     snap_response = requests.get(f"http://{IP_ADDRESS}/api/v1/cameras/snap", headers=HEADERS)
     snap_response.raise_for_status()
 
-    if previous_snapshot != snap_response.content:
+    if previous_snapshot == snap_response.content:
+        if DEBUG==True: print(f"{this_time()} Misma captura, esperamos {sleep_time} segundos", flush=True)
+        return
+
+    else:
         previous_snapshot = snap_response.content
 
         time_printing = job_data.get("time_printing")
@@ -75,16 +72,8 @@ def download_snapshot():
         print(f"{this_time()} Captura obtenida y almacenada en {file_name}", flush=True)
         sleep_time = SLEEP_TIME
 
-    else:
-        if DEBUG == 1:
-            print(f"{this_time()} Misma captura, esperamos {sleep_time} segundos", flush=True)
-
-if initialize() is False:
-    print(f"{this_time()} Parece que no hay conexión con la impresora", flush=True)
-    sleep_time = 300
-    exit()
-
 while True:
+
     try:
         download_snapshot()
         time.sleep(sleep_time)
