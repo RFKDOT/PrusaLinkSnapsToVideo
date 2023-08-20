6ecfb67c624c25935b1dd3759a72d688e2f71743
RFK <rafakroma@gmail.com>
2023-08-20 16:28:13 +0200

    Some Changes and adecuations

--- PrusaLinkGetSnaps.py
+++ PrusaLinkGetSnaps.py
@@ -1,26 +1,33 @@
 import os
 import time
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
 
-IP_ADDRESS = os.getenv("PRINTER_IP")
-API_KEY = os.getenv("API_KEY")
-HEADERS = {"X-Api-Key": API_KEY}
-SLEEP_TIME = 1
+IP_ADDRESS = os.getenv("PRUSALINK_IP")
+API_KEY = os.getenv("PRUSALINK_API_KEY")
+DEBUG = os.getenv("DEBUG", 1)
+SLEEP_TIME = int(os.getenv("SLEEP_TIME", 1))
 
+HEADERS = {"X-Api-Key": API_KEY}
+
+sleep_time = SLEEP_TIME
 previous_snapshot = None
 snapshot_counter = 0
+
+def this_time():
+    now = datetime.now()
+    return now.strftime("%d/%m/%Y %H:%M:%S")
 
 def initialize():
     try:
         info_response = requests.get(f"http://{IP_ADDRESS}/api/v1/info", headers=HEADERS)
         info_response.raise_for_status()
-        # print(info_response.json(), flush=True)
         return True
     except Exception as e:
-        print(f"Error: {e}", flush=True)
+        print(f"{this_time()} Error: {e}", flush=True)
         return False
 
 def format_time(seconds):
@@ -31,13 +38,14 @@ def format_time(seconds):
 
 def download_snapshot():
     global previous_snapshot
-
+    global sleep_time
 
     job_response = requests.get(f"http://{IP_ADDRESS}/api/v1/job", headers=HEADERS)
     job_response.raise_for_status()
 
     if job_response.status_code != 200:
-        print(f"Parece que en este momento no se está imprimiendo nada", flush=True)
+        print(f"{this_time()} Parece que en este momento no se está imprimiendo nada", flush=True)
+        sleep_time = 120
         return
 
     # print(job_response.json())
@@ -64,18 +72,23 @@ def download_snapshot():
         with open(os.path.join(os.getcwd(), file_name), "wb") as file:
             file.write(snap_response.content)
 
-        print(f"Captura obtenida y almacenada en {file_name}", flush=True)
+        print(f"{this_time()} Captura obtenida y almacenada en {file_name}", flush=True)
+        sleep_time = SLEEP_TIME
+
+    else:
+        if DEBUG == 1:
+            print(f"{this_time()} Misma captura, esperamos {sleep_time} segundos", flush=True)
 
 if initialize() is False:
-    print(f"Parece que no hay conexión con la impresora.", flush=True)
+    print(f"{this_time()} Parece que no hay conexión con la impresora", flush=True)
+    sleep_time = 300
     exit()
 
 while True:
-
     try:
         download_snapshot()
-        time.sleep(SLEEP_TIME)
+        time.sleep(sleep_time)
 
     except Exception as e:
         print(f"Error: {e}", flush=True)
-        time.sleep(SLEEP_TIME)
+        time.sleep(sleep_time)
